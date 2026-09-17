782be56cea64bc0cc159053cd96030eca792f849
FryPots <martinezserafingilbertoisai@gmail.com>
2026-09-17 15:17:42 -0600

    Replace filter functionality with createCards script; update styles for courses layout and responsiveness

---
+++ scripts/createCards.js
@@ -0,0 +1,57 @@
+const fetchCourses = async () => {
+    try {
+        const response = await fetch("/data/courses.json");
+        const data = await response.json();
+
+        return data.courses;
+
+    } catch (error) {
+        console.error("Error fetching courses:", error);
+    }
+};
+
+const createCard = (course, code) => {
+    const card = document.createElement("div");
+    const cardContent = document.createElement("p");
+
+    cardContent.textContent = `${course} ${code}`;
+    card.classList.add("courseCard");
+    card.classList.add(course);
+
+    card.appendChild(cardContent);
+
+    return card;
+};
+
+const createCards = async (cardCollection) => {
+    const container = document.querySelector(".courses-container");
+    try {
+        const courses = await fetchCourses();
+        
+        container.innerHTML = "";
+        courses.forEach(course => {
+            if (!cardCollection || cardCollection == course.class){
+                const card = createCard(course.class, course.code);
+                container.appendChild(card);
+            }
+        });
+
+    } catch (error) {
+        console.error("Error creating cards:", error);
+    }
+};
+
+createCards();
+
+document.getElementById("all-button").addEventListener("click", () => {
+    createCards();
+});
+
+
+document.getElementById("cse-button").addEventListener("click", () => {
+    createCards("CSE");
+});
+
+document.getElementById("wdd-button").addEventListener("click", () => {
+    createCards("WDD");
+});
